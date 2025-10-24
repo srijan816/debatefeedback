5179e5375ea11fb00a2dc8654c4da5e23ceb0010
Srijan <srijan8166@gmail.com>
2025-10-24 15:52:16 +0800

    Initial Commit

---
+++ DebateFeedback/DebateFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebateFeedbackApp.swift
+//  DebateFeedback
+//
+//  Created by Srijan on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct DebateFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DebateFeedbackTests/DebateFeedbackTests.swift
@@ -0,0 +1,17 @@
+//
+//  DebateFeedbackTests.swift
+//  DebateFeedbackTests
+//
+//  Created by Srijan on 10/24/25.
+//
+
+import Testing
+@testable import DebateFeedback
+
+struct DebateFeedbackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
